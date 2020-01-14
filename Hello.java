5cd84cddca6cf9863306b4e6dcc4b8b07039e5b4
itwillclass5 <59531538+itwillclass5@users.noreply.github.com>
2020-01-14 23:07:46 +0900

    Update Hello.java
    Add lines( System.out.println("GiirrrMaster add line2");)

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
     public static void main(String [] args){
         System.out.println("Hello Github");
         
-        System.out.println("GiirrrMaster add line");
-        
+        System.out.println("GiirrrMaster add line1");
+        System.out.println("GiirrrMaster add line2");
     }
 }
