9d371a69aa5806b81937ec7f08dd0ed581b7d46c
rlaxodud4774 <67987829+rlaxodud4774@users.noreply.github.com>
2020-07-20 15:28:53 +0900

    hello.java
    Add lien ( System.out.println("GiirrrMaster add line");)

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello{
     public static void main(String [] args){
         System.out.println("Hello Github");
         
+        
         System.out.println("GiirrrMaster add line");
         
     }
