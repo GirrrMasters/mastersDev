e55f5f971be255b2d6d097f976cbcb53965ac22a
Kim Changyeul <51772169+changyeulKim@users.noreply.github.com>
2022-10-11 15:10:11 +0900

    Test Commit Job
    Add System.out.println("Hello Github Test");

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@ public class Hello{
     public static void main(String [] args){
         System.out.println("Hello Github");
         
+        System.out.println("Hello Github Test");
+        
         System.out.println("GiirrrMaster add line");
         
     }
